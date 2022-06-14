f5a0c48e853bd4089c188bef8d480935dbac9742
Emon <contact.emonkhan@gmail.com>
2022-06-14 14:48:29 +0600

    Create Test.cpp

---
+++ Sublime_Setup/Test.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int TT, n;
+    cin >> TT;
+    while(TT--){
+        cin >> n;
+        vector<int> v(n);
+        for (int i = 0; i < n; ++i)cin >> v[i];
+    }
+}
