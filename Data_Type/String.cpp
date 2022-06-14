9b650b9972ae61ff03d90cc39354e8ae0ea13c83
Emon <schrodingersrats@gmail.com>
2022-06-14 14:37:38 +0600

    Add files via upload

---
+++ Data_Type/String.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string s = "achha tik ache";
+    int z = 1234;
+    //string sb = s.substr(0, 5);
+    //getline(cin, s);
+    //transform(s.begin(), s.end(), s.begin(), ::toupper);
+    //transform(s.begin(), s.end(), s.begin(), ::tolower);
+    //reverse(s.begin(), s.end());
+    //remove(s.begin(), s.end(), ' ');
+    //cout << string(5, 'A');
+    //s.push_back('e');
+    //s.pop_back();
+    //s.resize(5);
+    //sss.erase(remove(sss.begin(), sss.end(), 'b'), sss.end());
+    string ss = to_string(z);
+    cout << ss[1] << '\n'
+}
